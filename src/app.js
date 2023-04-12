4d323c794db8c21e8dd117570710343db08802f4
Yesbany Ramirez <yramirezalcedo@hotmail.com>
2023-04-11 22:56:54 -0500

    files with resolved code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,36 @@
 const express = require("express")
-
+require('dotenv').config()
 const { port } = require('../config')
 
+const productRouter = require('./products/products.router')
+
+const db = require('./utils/database')
 const app = express()
 
+
+//? Validar la conexión 
+
+db.authenticate()
+    .then(() => console.log('Database Authenticated!'))
+    .catch(err => console.log(err))
+
+db.sync()
+    .then(() => console.log('Database Synced!'))
+    .catch(err => console.log(err))
+
+//? conexión
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Server OK', 
+        myMessage: process.env.MESSAGE, 
+        myPort: process.env.PORT
+    })
+})
+
+app.use('/api/v1/products', productRouter)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
--- src/models/products.models.js
+++ src/models/products.models.js
@@ -1,4 +1,29 @@
-const Products = 'hola :D'
+const {DataTypes} = require('sequelize')
+
+const db = require('../utils/database')
+
+const Products = db.define('products', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    },
+    stock: {
+        type: DataTypes.INTEGER
+    }
+})
 
 module.exports = Products
 
